80e94e0112fc2be7d4bc23c59ac68d517ce5afc7
Tim Caswell <tim@creationix.com>
2015-03-11 16:49:51 -0500

    Start it

---
+++ package.lua
@@ -0,0 +1,10 @@
+return {
+  name = "luvit/blog.luvit.io",
+  version = "0.0.1",
+  private = true,
+  dependencies = {
+    "luvit/require",
+    "luvit/pretty-print",
+    "creationix/hoedown",
+  }
+}
