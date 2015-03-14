b6bd22e3adc82d145f51c0a167ff42c87a673408
Tim Caswell <tim@creationix.com>
2015-03-13 23:53:07 -0500

    Switch to cm for highlighting to use my notebook theme

--- libs/controllers/index.lua
+++ libs/controllers/index.lua
@@ -3,7 +3,7 @@ local loadIndex = require('models/index')
 
 return function (req, res, go)
   local articles = loadIndex()
-  res.body = render("index", {title="Index", articles=articles})
+  res.body = render("index", {title="Luvit Blog - Index", articles=articles})
   res.code = 200
   res.headers["Content-Type"] = "text/html"
 end
