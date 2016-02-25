9999f058272517c8707ceb9dd76bf01a7c2a5881
Tim Caswell <tim@creationix.com>
2016-02-25 15:37:24 -0600

    Update dependencies

--- package.lua
+++ package.lua
@@ -1,19 +1,19 @@
 return {
   name = "luvit/blog.luvit.io",
-  version = "0.0.1",
+  version = "0.0.2",
   private = true,
   dependencies = {
     -- hoedown ffi bindings for fast markdown compiling
-    "creationix/hoedown@1.1.0",
+    "creationix/hoedown@1.1.2",
     -- Web server framework
-    "creationix/weblit-app@0.2.6",
+    "creationix/weblit-app@2.1.0",
     -- Core plugin for proper http headers
-    "creationix/weblit-auto-headers@0.1.2",
+    "creationix/weblit-auto-headers2.0.1",
     -- Serve static files from disk
-    "creationix/weblit-static@0.2.4",
+    "creationix/weblit-static2.0.0",
     -- In-memory caching of http responses
-    "creationix/weblit-etag-cache@0.1.1",
+    "creationix/weblit-etag-cache2.0.0",
     -- Basic logger to stdout
-    "creationix/weblit-logger@0.1.1",
+    "creationix/weblit-logger2.0.0",
   }
 }
