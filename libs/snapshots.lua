b398d7886efb5fe56678d05afa4137cd81c204b6
Tim Caswell <tim@creationix.com>
2016-04-26 15:31:17 -0500

    Clean up snapshot output format

--- libs/snapshots.lua
+++ libs/snapshots.lua
@@ -1,4 +1,3 @@
-local jsonStringify = require('json').stringify
 local snapshot = require('snapshot')
 local i = 0
 local S1 = snapshot()
@@ -15,24 +14,30 @@ return function (_, res)
   for k, v in pairs(potential) do
     if S2[k] then
       c = c + 1
-      details[c] = {tostring(k), v}
+      details[c] = v
     end
   end
+  table.sort(details)
   potential = {}
+  local total = 0
+  local pot = 0
   for k,v in pairs(S2) do
+    total = total + 1
     if S1[k] == nil then
+      pot = pot + 1
       potential[k] = v
     end
   end
   S1 = S2
   i = i + 1
   res.code = 200
-  res.headers["Content-Type"] = "application/json"
-  res.body = jsonStringify{
-    id = i,
-    potential_count = c,
-    potential = details,
-    lua_heap = memoryUsed,
-  }
+  res.headers["Content-Type"] = "text/plain"
+  res.body = "id: " .. i ..
+    "\nheap bytes: " .. memoryUsed ..
+    "\nheap count: " .. total ..
+    "\npotential count: " .. pot ..
+    "\nleak count: " .. c ..
+    "\n\n" .. table.concat(details, "\n") ..
+    "\n"
 
 end
