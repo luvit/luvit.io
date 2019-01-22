cbb6172b0b97e3ba66aa6885f3be23ce6081a7f3
Tim Caswell <tcaswell@magicleap.com>
2019-01-22 11:32:46 -0600

    Make sure to use latest weblit-static

--- package.lua
+++ package.lua
@@ -1,6 +1,6 @@
 return {
   name = "luvit/blog.luvit.io",
-  version = "0.0.2",
+  version = "0.0.3",
   private = true,
   dependencies = {
     -- hoedown ffi bindings for fast markdown compiling
@@ -10,7 +10,7 @@ return {
     -- Core plugin for proper http headers
     "creationix/weblit-auto-headers@2.0.1",
     -- Serve static files from disk
-    "creationix/weblit-static@2.0.0",
+    "creationix/weblit-static@2.2.1",
     -- Basic logger to stdout
     "creationix/weblit-logger@2.0.0",
   }
