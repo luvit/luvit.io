7b3f06dd068f0177d0148a3f6d16c7e8ef6a476f
RDW <duckwhale@mail.ru>
2021-09-02 08:41:36 +0200

    Replace default favicon with the one from the luvit.io website

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -9,7 +9,7 @@ module.exports = {
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
   organizationName: 'luvit', // Usually your GitHub org/user name.
   projectName: 'luvit', // Usually your repo name.
   themeConfig: {
