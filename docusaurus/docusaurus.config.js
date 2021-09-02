101bda07b7cdfcf4b6ee8d2ef788e95f75bae2c8
RDW <duckwhale@mail.ru>
2021-09-02 07:58:08 +0200

    Replace default website title and tagline

--- docusaurus/docusaurus.config.js
+++ docusaurus/docusaurus.config.js
@@ -3,9 +3,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'Luvit',
+  tagline: 'Asynchronous I/O for Lua',
+  url: 'https://luvit.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
