d0da934996b1c3eb9cade1d654c817bf1eb92fda
RDW <duckwhale@mail.ru>
2021-09-03 05:20:43 +0200

    Split landing page introductory text for increased readability

--- docusaurus/src/components/HomepageFeatures.js
+++ docusaurus/src/components/HomepageFeatures.js
@@ -69,6 +69,8 @@ export default function HomepageFeatures() {
       <div className="hero-summary-blurb">
         <p>
           Luvit is a Lua runtime built on <a href="http://luajit.org/luajit.html" target="_blank">Mike Pall's LuaJIT engine</a>.
+        </p>
+        <p>
           It combines the asynchronous I/O library <a href="https://docs.libuv.org" target="_blank">libuv</a>,
           which also powers JavaScript's <a href="https://nodejs.org/">NodeJS</a>,
           with useful general-purpose libraries and tools that can help you create complex Lua-based applications with ease.
